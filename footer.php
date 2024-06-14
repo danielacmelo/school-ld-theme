f37934491d7fa31fd4f9d29d2dbbda609e260bf5
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-06-13 17:51:04 -0700

    Style: Add mixins,  update colors, body, links, media, headings, footer.

--- footer.php
+++ footer.php
@@ -12,46 +12,47 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-        <?php
-			if (has_custom_logo()) : ?>
-				<div class="footer-logo">
-					<a href="<?php echo esc_url(home_url('/')); ?>">
-						<?php the_custom_logo(); ?>
-					</a>
-				</div>
-				
-				<?php endif; ?>
+		<div class="footer-content">
+            <?php
+                if (has_custom_logo()) : ?>
+                    <div class="footer-logo">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">
+                            <?php the_custom_logo(); ?>
+                        </a>
+                    </div>
+                    
+                    <?php endif; ?>
 
-				<section class="footer-credits">
-					<h2>Credits</h2>
-					<p>Created by: 
-                        <strong><a href="https://wp.bcitwebdeveloper.ca/">Dani Melo & Lianrejo Sepnio</a></strong></p>
-					<p>Photos courtesy of:
-                        <strong><a href="https://burst.shopify.com">Burst</a><strong></p>
-				</section>
+                    <section class="footer-credits">
+                        <h2>Credits</h2>
+                        <p>Created by: 
+                            <strong><a href="https://wp.bcitwebdeveloper.ca/">Dani Melo & Lianrejo Sepnio</a></strong></p>
+                        <p>Photos courtesy of:
+                            <strong><a href="https://burst.shopify.com">Burst</a><strong></p>
+                    </section>
 
-				<?php
+                    
+                    <?php
 
-                // Get the footer menu
-				
-				$menu_location = 'footer-menu';
-				$locations = get_nav_menu_locations();
-				$menu_id = $locations[$menu_location];
-				$menu = wp_get_nav_menu_object($menu_id);
-				$menu_name = $menu->name;
+                    // Get the footer menu
+                    
+                    $menu_location = 'footer-menu';
+                    $locations = get_nav_menu_locations();
+                    $menu_id = $locations[$menu_location];
+                    $menu = wp_get_nav_menu_object($menu_id);
+                    $menu_name = $menu->name;
 
-                // Display the footer menu
-				wp_nav_menu(
-					array(
-						'items_wrap' => '<h2>' . esc_html($menu_name) . '</h2><ul id="%1$s" class="%2$s">%3$s</ul>',
-						'theme_location' => $menu_location,
-						'menu_id'        => 'footer-menu',
-					)
-				);
-				
-				?>
-		</div><!-- .site-info -->
+                    // Display the footer menu
+                    wp_nav_menu(
+                        array(
+                            'items_wrap' => '<h2>' . esc_html($menu_name) . '</h2><ul id="%1$s" class="%2$s">%3$s</ul>',
+                            'theme_location' => $menu_location,
+                            'menu_id'        => 'footer-menu',
+                        )
+                    );
+                    
+                    ?>
+		</div><!-- .footer-content -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,45 +10,47 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-aos="fade-up" >
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+   <div class="news-article"> 
+      <header class="entry-header">
+         <?php
+         if ( is_singular() ) :
+            the_title( '<h1 class="entry-title">', '</h1>' );
+         else :
+            the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+         endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				school_ld_theme_posted_on();
-				school_ld_theme_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+         if ( 'post' === get_post_type() ) :
+            ?>
+            <div class="entry-meta">
+               <?php
+               school_ld_theme_posted_on();
+               school_ld_theme_posted_by();
+               ?>
+            </div><!-- .entry-meta -->
+         <?php endif; ?>
+      </header><!-- .entry-header -->
 
-	<?php school_ld_theme_post_thumbnail(); ?>
+      <?php school_ld_theme_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-        if (is_single() ) {     
-		    the_content();
-        }else{
+      <div class="entry-content">
+         <?php
+         if ( is_single() ) {     
+            the_content();
+         } else {
             the_excerpt();
-        }
+         }
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'school-ld-theme' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+         wp_link_pages(
+            array(
+               'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'school-ld-theme' ),
+               'after'  => '</div>',
+            )
+         );
+         ?>
+      </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php school_ld_theme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+      <footer class="entry-footer">
+         <?php school_ld_theme_entry_footer(); ?>
+      </footer><!-- .entry-footer -->
+   </div> <!-- Close the div here -->
 </article><!-- #post-<?php the_ID(); ?> -->
